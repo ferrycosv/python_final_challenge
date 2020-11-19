b5a47eea4e9398de2e311380d206a29d45fad61c
Francisco Fernandez <ferrycosv@gmail.com>
2020-11-19 13:22:48 +0100

    Completed CLI program with plotting funtionality

--- challenge.py
+++ challenge.py
@@ -2,8 +2,9 @@ import georasters as gr
 import os
 import matplotlib.pyplot as plt
 from dms2dec.dms_convert import dms2dec
+import plotly.graph_objects as go
 import pyproj
-
+import numpy as np
 
 print('Welcome to my python challenge CLI program to plot a DSM given a set of coordinates in DMS')
 # Read the GeoTiff file and extract properties
@@ -23,7 +24,13 @@ degrees = input('Please input the degrees value (number ex. 51)\n')
 minutes = input('Please input the minutes value (number ex. 12)\n')
 seconds = input('Please input the minutes value (number ex. 31.8)\n')
 heading = input('Please input the heading (options N, S)\n')
-latitude = dms2dec(f'''{degrees}°{minutes}'{seconds}"{heading}''')
+
+# degrees = 51
+# minutes = 12
+# seconds = 34.1
+# heading = 'N'
+latitude_str = f'''{degrees}°{minutes}'{seconds}"{heading}'''
+latitude = dms2dec(latitude_str)
 
 # Ask user to input longitude value in DMS and calculate decimal value
 print('Now please input the longitude coordinates...')
@@ -32,7 +39,12 @@ minutes = input('Please input the minutes value (number ex. 13)\n')
 seconds = input('Please input the minutes value (number ex. 27.4)\n')
 heading = input('Please input the heading (options W, E)\n')
 
-longitude = dms2dec(f'''{degrees}°{minutes}'{seconds}"{heading}''')
+# degrees = 3
+# minutes = 13
+# seconds = 22.5
+# heading = 'E'
+longitude_str = f'''{degrees}°{minutes}'{seconds}"{heading}'''
+longitude = dms2dec(longitude_str)
 
 # Transform the decimal coordinates to the projection extracted from the file properties
 transformer = pyproj.Transformer.from_crs('epsg:4326', f'{crs_authority[0]}:{crs_authority[1]}')
@@ -41,17 +53,34 @@ print(f'The transformed values to {crs_authority[0]}:{crs_authority[1]} are [X:{
 
 # Ask user to provide a radius to filter raster file
 radius = int(input(f'Please input the radius to filter in {units}s (integer ex. 100)\n'))
+# radius = 50
 
 # Extract the area of interest from the raster file
 trimmed_file = complete_file.extract(lambert_x, lambert_y, radius=radius)
-trimmed_file.plot()
+# trimmed_file.plot()
 
 # Show to user the trimmed raster file
-plt.show()
+# plt.show()
 
 # Convert raster file to pandas dataframe
 df = trimmed_file.to_pandas()
-print(df.head())
+
+# Process the data frame to create the inputs for the surface plot
+z = []
+x = df['col'].unique()
+y = df['row'].unique()
+for i in x:
+    row = df.query(f'col == {i}')['value'].to_list()
+    z.append(row)
+z = np.array(z)
+
+# Plot the meshgrid from the 3D data points
+layout = go.Layout(scene=dict(aspectmode='data'),
+                   title=f'Center coordinates: [{latitude_str}, {longitude_str}] Radius = {radius} {units}s',
+                   autosize=True)
+fig = go.Figure(data=[go.Surface(x=x, y=y, z=z, colorscale='dense', showlegend=False)], layout=layout)
+fig.show()
+
 print('Thank you for using my program, I hope it was useful to you!')
 print('Created by: Francisco Fernandez')
 print('Copyright 2020')
